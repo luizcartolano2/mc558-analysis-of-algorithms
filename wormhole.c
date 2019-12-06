6027390270986a546a59428f00dc2c5362cf6669
Luiz Cartolano <duducartolano@gmail.com>
2019-12-06 16:10:00 -0300

    finished proj3

--- wormhole.c
+++ wormhole.c
@@ -1,15 +1,16 @@
 /**
- * @file puzzle.c
+ * @file wormhole.c
  * @author Luiz Eduardo Cartolano - 183012
  * @date 6 Dec 2019
  * @brief 
  *
- * 
- * 
- * 
- * 
- * 
- * 
+ * All wormholes have a constant time difference between their input and output. 
+ * For example, a given wormhole may cause the person passing through it to stop 15 years in the future. 
+ * Ellie Arroway, a brilliant physicist living on Earth, wants to study the Big Bang using wormholes. 
+ * The scientist wants to achieve a sequence of wormholes that allows her to go far enough in the past.
+ * Your goal is to reach the beginning of the universe and see the Big Bang with your own eyes.
+ * Write a program to find out if your venture will be successful.
+ *
  * @see https://susy.ic.unicamp.br:9999/mc558a/03/enunc.html
  */
 
@@ -47,9 +48,11 @@ int main(int argc, char const *argv[]) {
     /* here we gonna read the number of elements and create the pointers vector */
     scanf("%d %d",&num_vertices, &num_arestas);
 
+    /* alocate the edges and distance vectors */
     edges = (struct Edge*) malloc(num_arestas * sizeof(struct Edge));
     dst = malloc((num_vertices) * sizeof(int));
 
+    /* Initialize the distance vertex from source */
     for (i = 0; i < num_vertices; ++i)
     	dst[i] = 999999999;
     dst[0] = 0;
@@ -57,7 +60,6 @@ int main(int argc, char const *argv[]) {
     /* here we read the graph edges and create them on the linked list (we are consideing
        an unoriented graph */
     for (i = 0; i < num_arestas; i++) {
-        // printf("%d\n", i);
         scanf("%d %d %d",&u,&v,&weight);
         edges[i].u = u;
         edges[i].v = v;
@@ -67,14 +69,16 @@ int main(int argc, char const *argv[]) {
         	neg_edge = 1;
     }
     
+    /* If all edges are positive it's impossible to go back on time */
     if (!neg_edge) {
     	printf("Impossivel\n");
     	return 0;
     }
 
+    /*********************/
     /* Exec Bellman-Ford */
-    
-    /* First we relax the vertex */
+    /*********************/
+    /* First we relax the vertex V-1 times */
     for (i = 0; i < num_vertices; i++) {
     	for (j = 0; j < num_arestas; j++) {
     		u = edges[j].u;
@@ -99,9 +103,12 @@ int main(int argc, char const *argv[]) {
 		}
     }
 
+    /* If the code gets here doesn't have neg cycles, so its impossible */
+    printf("Impossivel\n");
+
+    /* Free the memory */
     free(edges);
     free(dst);
 
-    printf("Impossivel\n");
     return 0;
 }
